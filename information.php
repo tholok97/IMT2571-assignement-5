28d7785017fbf0da456aeefe86c3c17919e32df7
Thomas Løkkeborg <thomahl@stud.ntnu.no>
2017-11-01 11:59:31 +0100

    added information class

---
+++ information.php
@@ -0,0 +1,14 @@
+<?php
+
+// Information contains the information in arrays
+class Information {
+    public skiers = array();  
+    public clubs = array();  
+    public seasons = array();  
+    public cities = array();  
+    public counties = array();  
+    public affiliations = array();  
+    public totals = array();  
+}
+
+?>
